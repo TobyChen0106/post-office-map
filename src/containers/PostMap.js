4accee1256b7e693e2359ffde7ba615920c93f26
TobyChen0106 <awesdjzo@gmail.com>
2020-07-05 21:47:11 +0800

    update

--- src/containers/PostMap.js
+++ src/containers/PostMap.js
@@ -194,9 +194,9 @@ class PostMap extends Component {
                     this.setState({ loading: false });
                 }
             );
-
+            alert("haha");
             navigator.permissions.query({ name: 'geolocation' }).then((permissionStatus) => {
-                alert(permissionStatus.state);
+                
                 if (permissionStatus.state === "prompt") {
                     this.createNotification("warning", "點一下授權", "卡伯郵局地圖需要您現在的位置以提供定位")
                 } else {
